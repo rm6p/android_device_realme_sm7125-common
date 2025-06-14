3ec5ac6fcaa4e24067a68aa9c7a282750ebbb9d8
shaenix <theshaenix@gmail.com>
2025-06-14 21:56:20 +0530

    Simplify FM2 section qcom.fmradio is now built by default

--- common.mk
+++ common.mk
@@ -166,9 +166,8 @@ PRODUCT_COPY_FILES += \
 
 #FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    qcom.fmradio
-
+    FM2 
+    
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
