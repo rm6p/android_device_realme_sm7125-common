fa0d126f18f3075082a802cf4ae8c8b1234ca7f1
shaenix <theshaenix@gmail.com>
2025-07-22 12:41:21 +0530

    sm7125-common: Switch to android.hardware.usb@1.3-service.dual_role_usb

--- common.mk
+++ common.mk
@@ -450,8 +450,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
+    android.hardware.usb@1.3-service.dual_role_usb 
+    
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
